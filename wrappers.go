ca5f7d4d14a649a84e22bc0e79ac9b3b6d1d8c45
Bogdan <mereu.etern@gmail.com>
2022-09-24 13:35:10 +0300

    - modified ReaderOf to return a Reader that follows the full read convention

--- wrappers.go
+++ wrappers.go
@@ -5,11 +5,26 @@ import (
 )
 
 type reader struct {
-	io.Reader
+	r io.Reader
 }
 
 func (x reader) Close() error {
+	if c, ok := x.r.(Closer); ok {
+		return c.Close()
+	}
 	return nil
+}
+
+func (x reader) Read(b []byte) (int, error) {
+	var (
+		o, n int
+		err  error
+	)
+	for o < len(b) && err == nil {
+		n, err = x.r.Read(b[o:])
+		o += n
+	}
+	return o, err
 }
 
 type writer struct {
@@ -20,12 +35,13 @@ func (x writer) Close() error {
 	return nil
 }
 
-// ReaderOf attaches a NoOp Close method to an io.Reader.
+// ReaderOf adapts an [io.Reader] to this package's full read convention.
+// If it is not a Closer, it will also get a NoOp Close method.
 func ReaderOf(r io.Reader) Reader {
 	return reader{r}
 }
 
-// WriterOf attaches a NoOp Close method to an io.Writer.
+// WriterOf attaches a NoOp Close method to an [io.Writer].
 func WriterOf(w io.Writer) Writer {
 	return writer{w}
 }
